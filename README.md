24a3265084f9aec437138e842edf7edbd60e6e34
OONULL <1224204958@qq.com>
2020-07-28 15:31:43 +0800

    修改: gamll-user 用户模块拆分为用户servier模块和用户web模块 添加: gmall-common-util 公共工具模块 gmall-web-util web 公共工具模块 gmall-user-seriver 用户servier模块 gmall-user-web 用户web模块

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# gmall
+# gmall 本地修改版本
 gmall-parent---父工程依赖  
 gmall-api---api接口  
-gmall-user---用户工程  
+gmall-common-utiol---公共工具模块  
+gmall-web-util---web公共工具模块
+# gmall-user---用户服务工程  
+gmall-user-service---用户服务的service层 8070  
+gmall-user-web---用户服务的web层 8080  
---
+++ gmall-user-web/src/main/java/com/sq/gmall/user/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.sq.gmall.user.controller;
+
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.sq.gmall.bean.UmsMember;
+import com.sq.gmall.bean.UmsMemberReceiveAddress;
+import com.sq.gmall.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Reference
+    private UserService userService;
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
+
+        return umsMemberReceiveAddresses;
+    }
+
+
+    @RequestMapping("getAllUser")
+    @ResponseBody
+    public List<UmsMember> getAllUser(){
+
+        List<UmsMember> umsMembers = userService.getAllUser();
+
+        return umsMembers;
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
+
+
+
+}
